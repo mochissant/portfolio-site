250c76446c35b8abb9c338d7935b740f4b114137
mochissant <mochissant@gmail.com>
2025-02-15 06:16:14 +0000

    check nect page

--- .next/server/chunks/ssr/_d5dee6._.js
+++ .next/server/chunks/ssr/_d5dee6._.js
@@ -420,12 +420,12 @@ var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist
 var __TURBOPACK__imported__module__$5b$project$5d2f$app$2f$pages$2f$ProjectDetail$2e$tsx__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__ = __turbopack_import__("[project]/app/pages/ProjectDetail.tsx [app-rsc] (ecmascript)");
 ;
 ;
-function ProjectDetailPage({ params }) {
+async function ProjectDetailPage({ params }) {
     return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$app$2f$pages$2f$ProjectDetail$2e$tsx__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["default"], {
         projectId: params.projectId
     }, void 0, false, {
         fileName: "[project]/app/work/[projectId]/page.tsx",
-        lineNumber: 5,
+        lineNumber: 11,
         columnNumber: 10
     }, this);
 }
