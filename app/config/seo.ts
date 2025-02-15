e91aeb4d07d5b94c9afd55afa5bcf6508853261d
mochissant <mochissant@gmail.com>
2025-02-15 06:05:13 +0000

    delete doubles

--- app/config/seo.ts
+++
@@ -1,9 +0,0 @@
-
-// SEO関連の設定を管理
-export const seoConfig = {
-  defaultTitle: 'ポートフォリオ',
-  defaultDescription: 'デザイナーのポートフォリオサイトです',
-  defaultOgImage: '/images/ogp.jpg',
-  siteUrl: 'https://your-portfolio.com',
-  titleTemplate: '%s | ポートフォリオ'
-}
