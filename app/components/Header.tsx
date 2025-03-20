9c92408abee79b5823053cbffb6bea6e2bae2606
mochissant <mochissant@gmail.com>
2025-03-20 18:51:35 +0900

    使わないもの

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,9 +1,9 @@
 'use client';
 import styles from "../styles/header.module.css";
-import {useState} from "react";
+//import {useState} from "react";
 
 export default function Header(){
-    const [isOpen, setIsOpen] = useState();
+    //const [isOpen, setIsOpen] = useState();
 
     return(
         <header className={styles.header}>
