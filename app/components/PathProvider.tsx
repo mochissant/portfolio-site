0e4935afbc4617c8bdaba0de1819206b38b0a184
mochissant <mochissant@gmail.com>
2025-03-20 18:14:06 +0900

    立ち上げ

---
+++ app/components/PathProvider.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+
+export default function PathProvider({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
---
+++ app/components/Top.tsx
@@ -0,0 +1,10 @@
+'use client';
+import {styles} from "../styles/home.module.css";
+
+export default function Top (){
+    return(
+     <div>
+        トップ
+     </div>   
+    );
+}
