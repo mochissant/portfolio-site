f90de765f066cb8dd8f3cf250ae90010e68e332a
mochissant <mochissant@gmail.com>
2025-03-21 22:16:25 +0900

    css読み込み

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import './styles/globals.css';  // 追加
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import PathProvider from "./components/PathProvider";
